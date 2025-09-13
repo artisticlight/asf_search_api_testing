629378a1d8ca06cb4e18a0cccc547563f33d88d5
Andrew Anderson <raanderson9@alaska.edu>
2025-09-12 18:42:14 -0700

    DS-4275 Fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asf_search as asf
+import asf_search.constants as c
 
-results = asf.search(polarization=asf.POLARIZATION.DUAL_VV)
+results = asf.search(polarization=c.POLARIZATION.DUAL_VV)
 print("Polarization using constants:", len(results))
 
 results = asf.search(polarization='Dual VV')
